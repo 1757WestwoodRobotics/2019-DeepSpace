64ab048905a1268f36f82fc5f0414ed14f404002
Sean Lendrum <21slendrum@wpsstudents.org>
2019-01-30 19:27:09 -0500

    Add compressor

---
+++ src/main/java/org/whsrobotics/subsystems/Pneumatics.java
@@ -0,0 +1,22 @@
+package org.whsrobotics.subsystems;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Pneumatics extends Subsystem {
+
+    Compressor compressor;
+    
+
+
+
+
+
+
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+}
