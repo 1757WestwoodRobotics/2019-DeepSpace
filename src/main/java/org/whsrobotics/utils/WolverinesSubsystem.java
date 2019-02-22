8798b9ccddd1eaafae5334f4fcad907235363469
Larry Tseng <lt222888@gmail.com>
2019-02-22 01:10:36 -0500

    Continue re-architecture of WolverinesSubsystem to add locks and less-harsh failure
    Signed-off-by: Larry Tseng <lt222888@gmail.com>

--- src/main/java/org/whsrobotics/utils/WolverinesSubsystem.java
+++ src/main/java/org/whsrobotics/utils/WolverinesSubsystem.java
@@ -1,6 +1,8 @@
 package org.whsrobotics.utils;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 import java.util.HashSet;
@@ -10,34 +12,76 @@ public abstract class WolverinesSubsystem extends Subsystem {
     private static final double PERIODIC_TIME = 0.5;
 
     private static HashSet<WolverinesSubsystem> subsystems;
+    private static HashSet<WolverinesSubsystem> failedSubsystems;
     private static Notifier notifier;
     private boolean isMissionCritical;
 
     static {
         subsystems = new HashSet<>();
+        failedSubsystems = new HashSet<>();
     }
 
     protected WolverinesSubsystem(boolean isMissionCritical) {
         this.isMissionCritical = isMissionCritical;
-        subsystems.add(this);
     }
 
     protected abstract void init(boolean onTestRobot);
 
     public static void initSubsystems(boolean onTestRobot, WolverinesSubsystem... subsystems) {
+        _initSubsystems(onTestRobot, subsystems);
+
+        if (notifier == null)
+            notifier = new Notifier(WolverinesSubsystem::defineReducedPeriodic);
+    }
+
+    public static void reInitSubsystem(boolean onTestRobot, WolverinesSubsystem subsystem) {
+        failedSubsystems.remove(subsystem);
+
+        if (subsystem.getCurrentCommand() != null)
+            subsystem.getCurrentCommand().cancel();     // TODO: Does this actually cancel the command?
+
+        _initSubsystems(onTestRobot, subsystem);
+
+        notifier.stop();
+        notifier = new Notifier(WolverinesSubsystem::defineReducedPeriodic);
+        beginReducedPeriodic();
+    }
+
+    private static void _initSubsystems(boolean onTestRobot, WolverinesSubsystem... subsystems) {
 
         for (WolverinesSubsystem ws : subsystems) {
             try {
                 ws.init(onTestRobot);
+                WolverinesSubsystem.subsystems.add(ws);
+
             } catch (Exception ex) {
-                // TODO: Report that the subsystem was not initializable, if mission critical -> stop the robot
-                // Remove capabilities and commands?
+
+                failedSubsystems.add(ws);
+
+                // Removes capabilities and sets an un-interruptable command that requires(subsystem) -> locks the Scheduler
+
+                (new Command() {
+
+                    {
+                        requires(ws);
+                        setInterruptible(false);    // TODO: Check if this actually locks the subsystem
+                        setRunWhenDisabled(true);
+                    }
+
+                    @Override
+                    protected boolean isFinished() {
+                        return false;
+                    }
+
+                }).start();
+
                 if (ws.isMissionCritical)
-                    throw new RuntimeException("**** ERROR: Unable to initialize " + ws.getName() + " ****");
+                    throw new RuntimeException("**** ERROR: Unable to initialize mission-critical " + ws.getName() + " ****");
+                else
+                    DriverStation.reportError("* WARNING: Unable to initialize non-mission-critical " + ws.getName() + " *", false);
+
             }
         }
-
-        notifier = new Notifier(WolverinesSubsystem::defineReducedPeriodic);
 
     }
 
@@ -56,7 +100,7 @@ public abstract class WolverinesSubsystem extends Subsystem {
                 ws.reducedPeriodic();
             }
         } catch (Exception ex) {
-            // TODO: Just report an error.
+            // TODO: Remove from HashSet and report an error. Give the option to reinitialize
         }
     }
 
