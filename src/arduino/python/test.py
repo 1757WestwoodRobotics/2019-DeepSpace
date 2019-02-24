926705737d079e9d93d1a51ef308068c67c4baaf
Chetan Gopal <cb_gopal@yahoo.com>
2019-02-24 12:56:56 -0500

    Minor modifications and clean up.

--- src/arduino/python/test.py
+++ src/arduino/python/test.py
@@ -1,11 +1,15 @@
+"""
+Test Driver program to test USB communication with Arduino. Use this as an example to communicate with Arduino in you Python code. Cummunication to Arduino using JSON and can be extened to do other things.
+"""
 import sys
 import json
 import arduinoControl
 
 # If Teensy or Arduino is connected to Mac or Linux uncomment line below and change port
-#arduino = arduinoControl.Arduino('/dev/cu.usbmodem52640001')
-#Teensy or Arduino connectd to PC
-arduino = arduinoControl.Arduino('COM13')
+arduino = arduinoControl.Arduino('/dev/cu.usbmodem52640001')
+
+#Teensy or Arduino connectd to PC on COM Ports. Modify COM13 tot he right COM Port
+#arduino = arduinoControl.Arduino('COM13')
 
 while 1:
 	val = input("Enter a color 0 to 6 to control LED color, 8 - read lidar, 9 - exit):")
