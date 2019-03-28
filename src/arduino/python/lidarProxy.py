8fc0e31854de71c79dfd61f8dac67884ce21b1ae
Chetan <cb_gopal@yahoo.com>
2019-03-28 07:38:50 -0400

    Cleaned up Lidar Proxy.

--- src/arduino/python/lidarProxy.py
+++ src/arduino/python/lidarProxy.py
@@ -10,12 +10,16 @@ import time
 from networktables import NetworkTables
 import arduinoControl
 
-# If Teensy or Arduino is connected to Mac or Linux uncomment line below and change port
+# If Teensy or Arduino is connected to Mac or Linux uncomment line below
+# and change port to the right on one. On Mac usually /dev/cu.* and on Linux
+# /dev/serial1 or /dev/serial/*
+
 #arduino = arduinoControl.Arduino('/dev/cu.usbmodem52640001')
 
 #Teensy or Arduino connectd to PC on COM Ports. Modify COM13 tot he right COM Port
 arduino = arduinoControl.Arduino('COM22')
 
+# Turn on the Green Light on the LED.
 command = '{ "sensor": "ringLight", "color": 0 }'
 print(command)
 cmd = json.loads(command)
@@ -28,9 +32,10 @@ arduino.write(json.dumps(cmd))
 # and update Arduino via serial port to move the slider pot.
 # Postion values are bound  to -512 to 512 by the arduino.
 ######################################################################
-NetworkTables.initialize(server="127.0.0.1")
+#NetworkTables.initialize(server="127.0.0.1")
+NetworkTables.initialize(server='10.17.57.2')
 ld = NetworkTables.getTable('Lidar')
-i = 0
+
 while True:
     try:
         command = '{ "sensor": "Lidar", "read": 1 }'
@@ -43,9 +48,8 @@ while True:
             json_data = json.loads(data)
             distance = json_data["front"]
             print "LIDAR Reading : ", distance, "mm"
-            ld.putNumber("distance", distance+i)
-            print "Sending LIDAR Reading : ", distance+i, "mm"
-            i = i+1
+            ld.putNumber("distance", distance)
+            print "Sending LIDAR Reading : ", distance, "mm"
         else:
             ld.putNumber("distance", -10.0)
         time.sleep(0.2)
